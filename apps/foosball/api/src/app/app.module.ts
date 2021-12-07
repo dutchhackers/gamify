cffce185a9b55b4f39b898fd9934eb430ad35462
Michael <mschilling@move4mobile.com>
2021-12-07 21:39:24 +0000

    implement nest core library (foosball)

--- apps/foosball/api/src/app/app.module.ts
+++ apps/foosball/api/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { DataModule } from '@foosball/data';
+import { CoreModule } from '@foosball/core';
 import { FoosballPlayerModule } from '@foosball/player';
 import { FoosballMatchModule } from '@foosball/match';
 
 @Module({
-  imports: [DataModule, FoosballPlayerModule, FoosballMatchModule],
+  imports: [DataModule, CoreModule, FoosballPlayerModule, FoosballMatchModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/foosball/core/README.md
@@ -0,0 +1,7 @@
+# foosball-core
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test foosball-core` to execute the unit tests via [Jest](https://jestjs.io).
