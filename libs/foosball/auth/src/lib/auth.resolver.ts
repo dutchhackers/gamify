a8a2a16c677df9c87a85a266b78ab2da1fb28223
Michael <mschilling@move4mobile.com>
2021-12-12 20:02:11 +0000

    add me-query to return current user

--- libs/foosball/auth/src/lib/auth.resolver.ts
+++ libs/foosball/auth/src/lib/auth.resolver.ts
@@ -1,12 +1,22 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { CtxUser } from './decorators/ctx-user.decorator';
 import { AuthLoginInput } from './dto/auth-login.input';
 import { AuthRegisterInput } from './dto/auth-register.input';
+import { GqlAuthGuard } from './guards/gql-auth.guard';
+import { User } from './models/user';
 import { UserToken } from './models/user-token';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(GqlAuthGuard)
+  @Query(() => User, { name: 'me' })
+  me(@CtxUser() user: User) {
+    return user;
+  }
 
   @Mutation(() => UserToken)
   login(@Args({ name: 'input', type: () => AuthLoginInput }) input: AuthLoginInput) {
