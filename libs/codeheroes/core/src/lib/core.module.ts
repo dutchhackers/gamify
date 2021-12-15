31ac9dd008fafc470f48b2647720371c579e8ef7
Michael <mschilling@move4mobile.com>
2021-12-15 20:59:29 +0000

    add core (codeheroes)

---
+++ libs/codeheroes/core/src/lib/core.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+// import { GraphQLModule } from '@nestjs/graphql'
+import { configuration } from './config/configuration'
+import { validationSchema } from './config/validation'
+import { CoreResolver } from './core.resolver'
+// import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+// import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    // GraphQLModule.forRoot({
+    //   playground: false,
+    //   plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    //   autoSchemaFile: join(process.cwd(), 'libs/codeheroes/core/src/schema.gql'),
+    // }),
+  ],
+  controllers: [],
+  providers: [CoreResolver],
+  exports: [],
+})
+export class CoreModule {}
