03adf20b8e8f8522c41e78b1432cc70875bbdc58
Michael <mschilling@users.noreply.github.com>
2021-12-16 21:27:56 +0100

    feat: setup codeheroes auth (#56)
    Codeheroes setup:
    
    * auth lib
    * core lib

--- apps/codeheroes/api/src/app/app.module.ts
+++ apps/codeheroes/api/src/app/app.module.ts
@@ -3,8 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CoreModule } from '@codeheroes/core';
+import { AuthModule } from '@codeheroes/auth';
+
 @Module({
-  imports: [],
+  imports: [CoreModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
