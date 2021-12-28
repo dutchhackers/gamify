803c469b61fa0e8cb635981badd6175d3610e9d4
Michael <mschilling@move4mobile.com>
2021-12-28 21:54:58 +0000

    set jwt secret optional

--- libs/foosball/core/src/lib/config/validation.ts
+++ libs/foosball/core/src/lib/config/validation.ts
@@ -3,5 +3,5 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
   PORT: Joi.number().default(3000),
-  JWT_SECRET: Joi.string().required(),
+  //JWT_SECRET: Joi.string().required(),
 });
