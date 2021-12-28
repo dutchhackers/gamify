90dd0bea6a0f245e4bdf5caa56920bf5f4ac13f7
Michael <mschilling@move4mobile.com>
2021-12-28 21:07:42 +0000

    improve codeheroes core config

--- libs/codeheroes/core/src/lib/config/configuration.ts
+++ libs/codeheroes/core/src/lib/config/configuration.ts
@@ -1,7 +1,4 @@
 export const configuration = () => ({
   environment: process.env.NODE_ENV,
-  port: parseInt(process.env.PORT || '3000', 10),
-  jwt: {
-    secret: process.env.JWT_SECRET || '_',
-  }
-})
+  port: parseInt(process.env.API_PORT || process.env.PORT || '3000', 10),
+});
--- libs/codeheroes/core/src/lib/core.module.ts
+++ libs/codeheroes/core/src/lib/core.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
-import { configuration } from './config/configuration'
-import { validationSchema } from './config/validation'
-import { CoreResolver } from './core.resolver'
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { configuration } from './config/configuration';
+import { validationSchema } from './config/validation';
+import { CoreResolver } from './core.resolver';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: join(process.cwd(), 'libs/codeheroes/core/.env'),
       isGlobal: true,
       load: [configuration],
       validationSchema,
