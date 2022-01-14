56595032e8bc7dc9034b32a25e48b8f43e19847b
Michael <mschilling@move4mobile.com>
2022-01-14 14:24:05 +0000

    add badge-resolver

--- libs/crm/badge/src/lib/badge.module.ts
+++ libs/crm/badge/src/lib/badge.module.ts
@@ -2,11 +2,12 @@ import { DataModule } from '@crm/data';
 import { Module } from '@nestjs/common';
 import { BadgeService, BadgeAwardedService } from './services';
 import { BadgesResolver } from './resolvers/badges.resolver';
+import { BadgeAwardedResolver } from './resolvers/badge-awarded.resolver';
 
 @Module({
   imports: [DataModule],
   controllers: [],
-  providers: [BadgeService, BadgesResolver, BadgeAwardedService],
-  exports: [BadgeService, BadgeAwardedService],
+  providers: [BadgeService, BadgesResolver, BadgeAwardedService, BadgeAwardedResolver],
+  exports: [BadgeService],
 })
 export class BadgeModule {}
---
+++ libs/crm/badge/src/lib/resolvers/badge-awarded.resolver.ts
@@ -0,0 +1,23 @@
+import { Badge, BadgeAwarded } from '@crm/core';
+// import { EmployeeService } from '@crm/employee';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { BadgeService } from '../services';
+
+@Resolver(() => BadgeAwarded)
+export class BadgeAwardedResolver {
+  constructor(
+    private readonly badgeService: BadgeService // , private readonly employeeService: EmployeeService
+  ) {}
+
+  @ResolveField('badge', () => Badge)
+  async getBadge(@Parent() parent: BadgeAwarded) {
+    const { badgeId } = parent;
+    return this.badgeService.findOneById(badgeId);
+  }
+
+  // @ResolveField('employee', () => Employee)
+  // async getEmployee(@Parent() parent: BadgeAwarded) {
+  //   const { employeeId } = parent;
+  //   return this.employeeService.findOneById(employeeId);
+  // }
+}
