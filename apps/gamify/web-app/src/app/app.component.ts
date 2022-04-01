1e1b3a05cbc672f9610f5a7a93912d766f7e54de
Sander <sander@tuenda.io>
2022-04-01 15:47:46 +0200

    feat: Gamify setup (#199)
    * Update nx from 13.7 to 13.9 and add gamify angular project
    
    * Add gamify nestjs api
    
    * Delete gamify web-app e2e

---
+++ apps/gamify/web-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coders-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gamify-web-app';
+}
