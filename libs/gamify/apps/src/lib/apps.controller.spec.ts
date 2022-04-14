00ec3aa834408fb9263d3d6d05492a5bbff19903
Sander <sander@tuenda.io>
2022-04-14 09:05:18 +0000

    Make apps.controller tests work

--- libs/gamify/apps/src/lib/apps.controller.spec.ts
+++ libs/gamify/apps/src/lib/apps.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppsController } from './apps.controller';
+import { AppsService } from './apps.service';
 
 describe('AppsController', () => {
   let controller: AppsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [AppsService],
       controllers: [AppsController],
     }).compile();
 
