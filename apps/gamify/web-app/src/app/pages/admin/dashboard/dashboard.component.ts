3b79a85bcc651e65f52358761081a62f73cbee1b
Sander <sander@tuenda.io>
2022-05-13 12:06:15 +0000

    Fix linting

--- apps/gamify/web-app/src/app/pages/admin/dashboard/dashboard.component.ts
+++ apps/gamify/web-app/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'coders-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class DashboardComponent {
 
 }
--- apps/gamify/web-app/src/app/shared/components/application-details-moderators/application-details-moderators.component.ts
+++ apps/gamify/web-app/src/app/shared/components/application-details-moderators/application-details-moderators.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'coders-application-details-moderators',
   templateUrl: './application-details-moderators.component.html',
   styleUrls: ['./application-details-moderators.component.scss']
 })
-export class ApplicationDetailsModeratorsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ApplicationDetailsModeratorsComponent {
 
 }
