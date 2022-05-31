689a8462815e0b9dae40d8c3d8273c1901f168f6
Sander <sander@tuenda.io>
2022-05-31 11:56:58 +0000

    Fix unit test

--- apps/gamify/web-app/src/app/services/auth.service.spec.ts
+++ apps/gamify/web-app/src/app/services/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
@@ -6,7 +7,10 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [HttpClient]
+    });
     service = TestBed.inject(AuthService);
   });
 
