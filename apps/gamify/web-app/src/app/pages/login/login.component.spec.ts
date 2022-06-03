622bfbc48904ed2c173c192271ef4868e9f9b482
Sander <sander@tuenda.io>
2022-06-03 09:06:23 +0000

    Fix unit-tests for login component

--- apps/gamify/web-app/src/app/pages/login/login.component.spec.ts
+++ apps/gamify/web-app/src/app/pages/login/login.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,6 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [HttpClient],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
