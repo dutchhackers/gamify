919af513ce80ae1b30defd4cf51983d0878981a0
Sander <sander@tuenda.io>
2022-06-27 09:56:30 +0000

    Show actual data in de UI

--- apps/gamify/web-app/src/app/pages/users/profile/profile.component.ts
+++ apps/gamify/web-app/src/app/pages/users/profile/profile.component.ts
@@ -6,6 +6,37 @@ import { AuthService } from '../../../services/auth.service';
 import { BadgesService } from '../../../services/badges.service';
 import { UsersService } from '../../../services/users.service';
 
+interface UserApplicationProfile {
+  application: Application,
+  userApplication: ApplicationUser,
+  badgesCount: {
+    BRONZE: number,
+    SILVER: number,
+    GOLD: number,
+    PLATINUM: number
+  },
+  obtainedBadges: { 
+    [key: number]: {
+      badge: Badge,
+      amount: number
+    }
+  },
+  unobtainedBadges: Badge[],
+
+  obtainedBadgesCount: number,
+  totalBadgesCount: number,
+  obtainedPercentage: number,
+}
+
+interface Stats {
+  totalBadges: number,
+  totalGames: number,
+  bronzeBadges: number,
+  silverBadges: number,
+  goldBadges: number,
+  platinumBadges: number,
+}
+
 @Component({
   selector: 'coders-profile',
   templateUrl: './profile.component.html',
@@ -25,30 +56,18 @@ export class ProfileComponent implements OnInit {
     amount: number
   }[] = [];
 
-  stats: {
-    totalBadges: number,
-    totalGames: number,
-    bronzeBadges: number,
-    silverBadges: number,
-    goldBadges: number,
-    platinumBadges: number,
+  stats: Stats = {
+    totalBadges: 0,
+    totalGames: 0,
+    bronzeBadges: 0,
+    silverBadges: 0,
+    goldBadges: 0,
+    platinumBadges: 0
   }
 
-  userApplications: {
-    application: Application,
-    userApplication: ApplicationUser,
-    badgesCount: {
-      bronze: number,
-      silver: number,
-      gold: number,
-      platinum: number
-    },
-    obtainedBadges: {
-      badge: Badge,
-      amount: number
-    }[],
-    unobtainedBadges: Badge[]
-  }[] = [];
+  userApplications: UserApplicationProfile[] = [];
+
+  user$ = this.authService.getUser$();
 
   constructor(
     private readonly authService: AuthService,
@@ -58,58 +77,115 @@ export class ProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const user = this.authService.getUser$().getValue();
+    const user = this.user$.getValue();
 
     const userApplications = this.usersService.listUserApplications$(user.id);
     const applications = this.applicationService.list$();
     const badges = this.badgesService.list$();
     const userBadges = this.usersService.listUserBadges$(user.id);
     
-
     userApplications.pipe(
       combineLatestWith(applications, badges, userBadges)
     ).subscribe(([userApplications, applications, badges, userBadges]) => {
+      this.userApplications = this.aggregateUserApplications(userApplications, applications, badges, userBadges);
+      console.log(this.userApplications);
+      this.stats = this.calculateStats(userApplications, userBadges);
+    });
+  }
 
+  private aggregateUserApplications(userApplications: ApplicationUser[], applications: Application[], badges: Badge[], userBadges: UserBadge[]): UserApplicationProfile[] {
+    return userApplications.map(userApplication => {
+      const application = applications.find(app => app.id === userApplication.applicationId);
+      const applicationBadges = badges.filter(badge => badge.applicationId === application.id);
+      const applicationUserBadges = userBadges.filter(userBadge => userBadge.badge.applicationId === application.id);
+      
+      const badgesCount = {
+        BRONZE: 0,
+        SILVER: 0,
+        GOLD: 0,
+        PLATINUM: 0
+      }
 
-      const blah = userApplications.map(userApplication => {
-        const application = applications.find(app => app.id === userApplication.applicationId);
-        const applicationBadges = badges.filter(badge => badge.applicationId === application.id);
-        const applicationUserBadges = userBadges.filter(userBadge => userBadge.badge.applicationId === application.id);
-        
-        const badgesCount = {
-          bronze: 0,
-          silver: 0,
-          gold: 0,
-          platinum: 0
-        }
-
-        const obtainedBadges: {
+      const obtainedBadges: {
+        [key: number]: {
           badge: Badge,
           amount: number
-        }[] = [];
+        }
+      } = {};
+      let obtainedBadgesCount = 0;
 
-        applicationUserBadges.forEach(userBadge => {
-          badgesCount[userBadge.badge.tier] += 1;
+      applicationUserBadges.forEach(userBadge => {
+        badgesCount[userBadge.badge.tier] += 1;
 
-          const badge = applicationBadges.find(badge => badge.id === userBadge.badgeId);
-          // obtainedBadges.push({
-          //   badge: badge,
-          //   // amount: userBadge.amount
-          // });
-        });
+        const badge = applicationBadges.find(badge => badge.id === userBadge.badgeId);
 
-        return {
-          application,
-          userApplication,
-          badgesCount
+        if (! badge) {
+          return;
+        }
+
+        if (obtainedBadges[badge.id]) {
+          obtainedBadges[badge.id].amount += 1;
+        } else {
+          obtainedBadgesCount++;
+          obtainedBadges[badge.id] = {
+            badge,
+            amount: 1
+          }
         }
       });
 
-      console.log(userApplications);
-      console.log(applications);
-      console.log(badges);
-      console.log(userBadges);
+
+      let obtainedPercentage = 0;
+      if (applicationBadges.length > 0) {
+        obtainedPercentage = Math.round((obtainedBadgesCount / applicationBadges.length) * 100);
+      }
+
+      const unobtainedBadges = applicationBadges.filter(badge => ! applicationUserBadges.find(userBadge => userBadge.badgeId === badge.id));
+
+      return {
+        application,
+        userApplication,
+        badgesCount,
+        obtainedBadges,
+        unobtainedBadges,
+        obtainedBadgesCount,
+        totalBadgesCount: applicationBadges.length,
+        obtainedPercentage: obtainedPercentage
+      }
     });
+  }
+
+  private calculateStats(userApplications: ApplicationUser[], userBadges: UserBadge[]): Stats {
+    const stats = {
+      totalBadges: 0,
+      totalGames: 0,
+      bronzeBadges: 0,
+      silverBadges: 0,
+      goldBadges: 0,
+      platinumBadges: 0
+    };
+
+    stats.totalGames = userApplications.length;
+    stats.totalBadges = userBadges.length;
+
+    userBadges.forEach(userBadge => {
+      switch (userBadge.badge.tier) {
+        case BadgeTier.BRONZE:
+          stats.bronzeBadges += 1;
+          break;
+        case BadgeTier.SILVER:
+          stats.silverBadges += 1;
+          break;
+        case BadgeTier.GOLD:
+          stats.goldBadges += 1;
+          break;
+        case BadgeTier.PLATINUM:
+          stats.platinumBadges += 1;
+          break;
+      }
+    });
+
+    return stats;
   }
 
 }
